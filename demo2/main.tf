87f1076cf9bcac43f0ca7cad46d71ca8a3a7c31a
santoshvarmaalt <121477094+santoshvarmaalt@users.noreply.github.com>
2022-12-28 11:26:41 +0530

    Update main.tf

--- demo2/main.tf
+++ demo2/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
+  cidr_block = "172.16.0.1/16"
 
   tags = {
     Name = "tf-example"
@@ -24,7 +24,7 @@ resource "aws_vpc" "my_vpc" {
 
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
+  cidr_block        = "172.16.10.1/24"
   availability_zone = "us-east-1a"
 
   tags = {
