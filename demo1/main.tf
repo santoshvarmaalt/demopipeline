48d69f6d880bed67c239e690f0e50909b2c4b0b9
santoshvarmaalt <121477094+santoshvarmaalt@users.noreply.github.com>
2022-12-28 11:25:12 +0530

    Update main.tf

--- demo1/main.tf
+++ demo1/main.tf
@@ -32,25 +32,25 @@ resource "aws_subnet" "my_subnet" {
   }
 }
 
-resource "aws_network_interface" "foo" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
+# resource "aws_network_interface" "foo" {
+#   subnet_id   = aws_subnet.my_subnet.id
+#   private_ips = ["172.16.10.100"]
 
-  tags = {
-    Name = "primary_network_interface"
-  }
-}
+#   tags = {
+#     Name = "primary_network_interface"
+#   }
+# }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-0b5eea76982371e91" # us-west-2
-  instance_type = "t2.micro"
+# resource "aws_instance" "foo" {
+#   ami           = "ami-0b5eea76982371e91" # us-west-2
+#   instance_type = "t2.micro"
 
-  network_interface {
-    network_interface_id = aws_network_interface.foo.id
-    device_index         = 0
-  }
+#   network_interface {
+#     network_interface_id = aws_network_interface.foo.id
+#     device_index         = 0
+#   }
 
-  credit_specification {
-    cpu_credits = "unlimited"
-  }
-}
+#   credit_specification {
+#     cpu_credits = "unlimited"
+#   }
+# }
